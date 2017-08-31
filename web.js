4568d1cdf7db9b6cedeac46ac331cf08a1823837
Alexa <alexadiaz@msn.com>
2017-08-30 23:46:33 -0400

    Agregar cors para request externos

--- web.js
+++ web.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const accion = require("./accion.js");
+
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Debe escribir una accion"));
 
