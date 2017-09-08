4c2f54fa88811bd4f53cf1b3814d45a7ce69e64a
Alexa <alexadiaz@msn.com>
2017-09-08 19:30:12 -0400

    rutas con informacion fija y no varibales

--- web.js
+++ web.js
@@ -5,50 +5,55 @@ const accion = require("./accion.js");
 
 app.use(cors());
 
-app.get("/:consultar", (req, res) => {
-   accion.consultar().then(respuesta => res.json( respuesta ));
-});
 
-app.get("/:completar_todo", (req, res) => {
-    accion.completar_todo().then(respuesta => res.json(respuesta));
-});
-
-app.get("/:pendiente_todo", (req, res) => {
-    accion.pendiente_todo().then(respuesta => res.json(respuesta));
-});
-
-app.get("/:borrar_completados", (req, res) => {
-    accion.borrar_completados().then(respuesta => res.json(respuesta));
-});
-
-app.get("/:ayuda", (req, res) => {
+app.get("/ayuda", (req, res) => {
     res.json(accion.ayuda());
 });
 
-app.get("/:insertar/:tar", (req, res) => {
+app.get("/consultar", (req, res) => {
+   accion.consultar().then(respuesta => res.json( respuesta ));
+});
+
+app.get("/completar_todo", (req, res) => {
+    accion.completar_todo().then(respuesta => res.json(respuesta));
+});
+
+app.get("/pendiente_todo", (req, res) => {
+    accion.pendiente_todo().then(respuesta => res.json(respuesta));
+});
+
+app.get("/borrar_completados", (req, res) => {
+    accion.borrar_completados().then(respuesta => res.json(respuesta));
+});
+
+
+
+app.get("/insertar/:tar", (req, res) => {
     let tar = req.params.tar;
     accion.insertar(tar).then(respuesta => res.json(respuesta));
 });
 
-app.get("/:completar/:tar", (req, res) => {
+app.get("/completar/:tar", (req, res) => {
     let tar = req.params.tar;
     accion.completar(tar).then(respuesta => res.json(respuesta));
 });
 
-app.get("/:borrar/:tar", (req, res) => {
+app.get("/borrar/:tar", (req, res) => {
     let tar = req.params.tar;
     accion.borrar(tar).then(respuesta => res.json(respuesta));
 });
 
-app.get("/:consultar_tarea/:tar", (req, res) => {
+app.get("/consultar_tarea/:tar", (req, res) => {
     let tar = req.params.tar;
     accion.consultar_tarea(tar).then(respuesta => res.json({ respuesta }));
 });
 
-app.get("/:renombrar/:tar/:ntar", (req, res) => {
-    let tar = req.params.tar;
-    let ntar = req.params.ntar;
-    accion.renombrar(tar, ntar).then(respuesta => res.json(respuesta));
+app.get("/renombrar", (req, res) => {
+    accion.renombrar(req.query.tarea, req.query.nueva).then(respuesta => res.json(respuesta));
 });
 
-app.listen(3000);
+app.get("/*", (req, res) => {
+    res.json("esa tal ruta " + req.originalUrl + " no existe!!!");
+});
+
+app.listen(3000, () => console.log("Restarting ..."));
