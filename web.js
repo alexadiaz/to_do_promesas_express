f3a1fee95978caa3836b28699537e2274ba5e1b2
Alexa <alexadiaz@msn.com>
2017-09-11 23:35:12 -0400

    cambiar metodos de get a post

--- web.js
+++ web.js
@@ -7,46 +7,45 @@ var bodyparser = require("body-parser");
 app.use(cors());
 app.use(bodyparser.json());
 
-app.GET("/ayuda", (req, res) => {
+app.get("/ayuda", (req, res) => {
     res.json(accion.ayuda());
 });
 
-app.GET("/consultar", (req, res) => {
+app.get("/consultar", (req, res) => {
    accion.consultar().then(respuesta => res.json( respuesta ));
 });
 
-app.POST("/completar_todo", (req, res) => {
+app.post("/completar_todo", (req, res) => {
     accion.completar_todo().then(respuesta => res.json(respuesta));
 });
 
-app.POST("/pendiente_todo", (req, res) => {
+app.post("/pendiente_todo", (req, res) => {
     accion.pendiente_todo().then(respuesta => res.json(respuesta));
 });
 
-app.POST("/borrar_completados", (req, res) => {
+app.post("/borrar_completados", (req, res) => {
     accion.borrar_completados().then(respuesta => res.json(respuesta));
 });
 
-app.POST("/insertar", (req, res) => {
+app.post("/insertar", (req, res) => {
     accion.insertar(req.body.tarea).then(respuesta => res.json(respuesta));
 });
 
-app.POST("/completar", (req, res) => {
+app.post("/completar", (req, res) => {
     accion.completar(req.body.tarea).then(respuesta => res.json(respuesta));
 });
 
-app.POST("/borrar", (req, res) => {
+app.post("/borrar", (req, res) => {
     accion.borrar(req.body.tarea).then(respuesta => res.json(respuesta));
 });
 
-app.get("/consultar_tarea/:tar", (req, res) => {
-    let tar = req.params.tar;
-    accion.consultar_tarea(tar).then(respuesta => res.json({ respuesta }));
+app.get("/consultar_tarea", (req, res) => {
+    accion.consultar_tarea(req.query.tarea).then(respuesta => res.json(respuesta));
 });
 
-app.get("/renombrar", (req, res) => {
-    accion.renombrar(req.query.tarea, req.query.nueva).then(respuesta => res.json(respuesta));
-});
+// app.get("/renombrar", (req, res) => {
+//     accion.renombrar(req.query.tarea, req.query.nueva).then(respuesta => res.json(respuesta));
+// });
 
 app.get("/*", (req, res) => {
     res.json("esa tal ruta " + req.originalUrl + " no existe!!!");
